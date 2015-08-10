37572b8c1489cf684d62bf88877a6eea76d99a5f
Leonardo Zanivan <leonardo.zanivan@gmail.com>
2015-08-10 00:57:43 -0300

    Initial commit

---
+++ protractor.conf.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports.config = {
+    baseUrl: 'http://localhost:9000',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+
+    specs: [
+        'test/e2e/**/*.js'
+    ],
+
+    multiCapabilities: [{
+        browserName: 'firefox'
+    }]
+
+};
